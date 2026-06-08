95d6e91df131ea9e706e62b5031c07cdf9d5ccf1
tanishaagarwal24-cmd <tanishaagarwal24@ifheindia.org>
2026-06-08 15:20:50 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,1 @@
-document.querySelector(".btn").addEventListener("click", () => {
-    document.getElementById("projects").scrollIntoView({behavior:"smooth"});
-});
+console.log("Portfolio Loaded 🚀");
