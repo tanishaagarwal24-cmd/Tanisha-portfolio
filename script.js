1387ce7af5f9bdfd5d77c08d885241abb68dd6d1
tanishaagarwal24-cmd <tanishaagarwal24@ifheindia.org>
2026-06-11 23:09:00 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-console.log("Portfolio Loaded 🚀");
+// Smooth reveal animation
+
+const sections = document.querySelectorAll(
+'.card, .project-card, .skill-box'
+);
+
+const observer = new IntersectionObserver((entries)=>{
+    entries.forEach(entry=>{
+        if(entry.isIntersecting){
+            entry.target.style.opacity='1';
+            entry.target.style.transform='translateY(0)';
+        }
+    });
+},{
+    threshold:0.2
+});
+
+sections.forEach(section=>{
+    section.style.opacity='0';
+    section.style.transform='translateY(40px)';
+    section.style.transition='all 0.8s ease';
+    observer.observe(section);
+});
+
+console.log("Tanisha Portfolio Loaded Successfully 🚀");
